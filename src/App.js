ff0a25f2fd43f4c9d21221011e731b77ef5cad34
Costantino Cavallo <c.cavallo@uario.com>
2019-01-21 13:38:45 +0100

    VideoList added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import SearchBar from './videos/components/searchbar.component';
+import VideoList from './videos/components/videolist.component';
 
 class App extends Component {
   render() { 
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="container">
         <div className="App">
            <SearchBar />
+           <VideoList />
         </div>
       </div>
     );
---
+++ src/videos/components/videolist.component.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class VideoList extends Component {
+    render() {
+        return <div>VideoList</div>
+    }
+}
+
+export default VideoList;
